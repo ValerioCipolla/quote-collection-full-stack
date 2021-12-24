8841e696bef7b07134ae75da121c54c4c5bf0bb9
valeriocipolla <valeriocipolla92@gmail.com>
2021-12-24 10:41:46 +0000

    connected to db successfully

---
+++ config.js
@@ -0,0 +1,7 @@
+export const dbInfo = {
+  dbhost: process.env.PGHOST,
+  dbname: process.env.PGDATABASE,
+  username: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  dbport: process.env.PGPORT,
+};
---
+++ scripts/index.js
@@ -0,0 +1,18 @@
+import pg from "pg";
+
+import { dbInfo } from "../config.js";
+
+const pool = new pg.Pool({
+  user: dbInfo.username,
+  host: dbInfo.dbhost,
+  database: dbInfo.dbname,
+  password: dbInfo.password,
+  port: dbInfo.dbport,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+export default function query(text, params) {
+  return pool.query(text, params);
+}
