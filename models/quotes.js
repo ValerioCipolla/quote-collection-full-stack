e4420d9c1418da21c4cdb4803ff9b9fd8387aa79
valeriocipolla <valeriocipolla92@gmail.com>
2021-12-26 12:26:36 +0000

    added handler for post and delete requests

--- models/quotes.js
+++ models/quotes.js
@@ -1,6 +1,21 @@
 import query from "../db/index.js";
 
 export async function getAllQuotes() {
-  const data = await query(`SELECT * FROM quotes`);
+  const data = await query(`SELECT * FROM quotes;`);
+  return data.rows;
+}
+
+export async function createQuote(quote) {
+  const data = await query(
+    `INSERT INTO quotes (quote, author) VALUES ($1, $2) RETURNING *;`,
+    [quote.quote, quote.author]
+  );
+  return data.rows;
+}
+
+export async function deleteQuoteById(id) {
+  const data = await query(`DELETE FROM quotes WHERE id = $1 RETURNING *;`, [
+    id,
+  ]);
   return data.rows;
 }
--- routes/quotes.js
+++ routes/quotes.js
@@ -1,13 +1,50 @@
 import express from "express";
-import { getAllQuotes } from "../models/quotes.js";
+import {
+  getAllQuotes,
+  createQuote,
+  deleteQuoteById,
+} from "../models/quotes.js";
 const router = express.Router();
 
-/* GET users listing. */
 router.get("/quotes", async function (req, res) {
   const result = await getAllQuotes();
+  if (result.length <= 0) {
+    res.status(404).json({
+      success: false,
+      message: "No quotes found :(",
+    });
+    return;
+  }
   res.json({
     success: true,
     message: `All quotes found :)`,
+    payload: result,
+  });
+});
+
+router.post("/quotes", async function (req, res) {
+  const quote = req.body;
+  const result = await createQuote(quote);
+  res.status(201).json({
+    success: true,
+    message: `quote created :)`,
+    payload: result,
+  });
+});
+
+router.delete("/quotes/:id", async function (req, res) {
+  const id = req.params.id;
+  const result = await deleteQuoteById(id);
+  if (result.length <= 0) {
+    res.json({
+      success: false,
+      message: `quote with id ${id} was not found`,
+    });
+    return;
+  }
+  res.json({
+    success: true,
+    message: `quote with id ${id} has been deleted`,
     payload: result,
   });
 });
