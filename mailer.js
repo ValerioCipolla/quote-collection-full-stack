3627138ed30ecb6df51cae4633c86b52274661fc
valeriocipolla <valeriocipolla92@gmail.com>
2021-12-31 09:52:56 +0000

    successfully created nodemailer to send e-mails. not integrated yey

---
+++ mailer.js
@@ -0,0 +1,28 @@
+import nodemailer from "nodemailer";
+import { emailInfo } from "./config.js";
+
+const transporter = nodemailer.createTransport({
+  service: "outlook",
+  auth: {
+    user: emailInfo.email_address,
+    pass: emailInfo.email_password,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+const mailOptions = {
+  from: emailInfo.email_address,
+  to: "valerio_cipolla@hotmail.it",
+  subject: "Sending Email using Node.js",
+  text: "That was easy!",
+};
+
+transporter.sendMail(mailOptions, function (error, info) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Email sent: " + info.response);
+  }
+});
