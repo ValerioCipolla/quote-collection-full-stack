559ccdbfe2ea67c987fce39d233686062a7330f6
valeriocipolla <valeriocipolla92@gmail.com>
2022-03-11 09:22:53 +0000

    fixed outlook issue

--- mailer.js
+++ mailer.js
@@ -2,7 +2,7 @@ import nodemailer from "nodemailer";
 import { emailInfo } from "./config.js";
 
 export const transporter = nodemailer.createTransport({
-  service: "outlook",
+  host: `smtp-mail.outlook.com`,
   auth: {
     user: emailInfo.email_address,
     pass: emailInfo.email_password,
